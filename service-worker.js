110c6d15f88cb826584a36284bec0bbe147938ac
laelmuler3-afk <laelmuler3@gmail.com>
2026-07-14 12:26:14 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
  * Envie o service-worker.js por último e aumente APP_VERSION.
  */
 
-const APP_VERSION = "1.4.1";
+const APP_VERSION = "1.4.2";
 const CACHE_PREFIX = "shinobi";
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${APP_VERSION}`;
@@ -24,12 +24,12 @@ const APP_SHELL = [
   "./manifest.json",
   "./data/catalogo-jutsus.json",
   "./css/app.css",
-  "./css/catalogo.css",
+  "./css/catalogo.css?v=1.4.2",
   "./js/01-core.js",
   "./js/02-runtime.js",
   "./js/03-images.js",
   "./js/04-jutsus.js",
-  "./js/09-catalogo.js",
+  "./js/09-catalogo.js?v=1.4.2",
   "./js/05-battle.js",
   "./js/06-inventory.js",
   "./js/07-profile.js",
