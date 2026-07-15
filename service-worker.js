23bbcced013948343c9df6db64500825cb4fe755
laelmuler3-afk <laelmuler3@gmail.com>
2026-07-15 20:30:06 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
  * Envie o service-worker.js por último e aumente APP_VERSION.
  */
 
-const APP_VERSION = "1.8.0";
+const APP_VERSION = "1.8.1";
 const CACHE_PREFIX = "shinobi";
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-${APP_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${APP_VERSION}`;
@@ -26,18 +26,19 @@ const APP_SHELL = [
   "./css/app.css",
   "./css/catalogo.css?v=1.4.2",
   "./css/regras-natureza.css?v=1.7.2",
-  "./css/batalha-viva.css?v=1.8.0",
+  "./css/batalha-viva.css?v=1.8.1",
   "./js/01-core.js",
   "./js/02-runtime.js",
   "./js/03-images.js",
   "./js/04-jutsus.js",
   "./js/09-catalogo.js?v=1.4.2",
-  "./js/05-battle.js?v=1.8.0",
+  "./js/05-battle.js?v=1.8.1",
   "./js/06-inventory.js",
   "./js/07-profile.js",
   "./js/08-update.js",
-  "./js/10-regras-natureza.js?v=1.7.2",
+  "./js/10-regras-natureza.js?v=1.8.1",
   "./js/11-batalha-ui.js?v=1.8.0",
+  "./js/12-efeitos-jutsus.js?v=1.8.1",
   "./assets/ui-background-main.jpg",
   "./assets/ui-notes-parchment.jpg",
   "./icon_192x192.png",
